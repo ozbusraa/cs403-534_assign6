0889aed07ea761292ed8271e1bb042973af29efd
ozbusraa <bbusraaozz@gmail.com>
2020-04-07 20:32:37 +0300

    Map and reduce functions are done

--- find_word_count.py
+++ find_word_count.py
@@ -4,7 +4,20 @@ from map_reduce import MapReduce
 # implement
 class FindWordCount(MapReduce):
     def Map(self, map_input):
-        return 5
 
-    def Reduce(self):
-        return 10
+        filename=map_input["filename"]
+        start_index=map_input["startno"]
+        end_index=map_input["endno"]
+
+        #read file
+        f= open(filename,"r",encoding='utf-8')
+        text=f.read()
+
+        line=text[start_index:end_index]
+        return len(line.split(' '))
+
+    def Reduce(self,reduce_input):
+        result=0
+        for i in reduce_input:
+            result+=i
+        return result
--- find_word_frequency.py
+++ find_word_frequency.py
@@ -3,7 +3,28 @@ from map_reduce import MapReduce
 
 class FindWordFrequency(MapReduce):
     def Map(self, map_input):
-        pass
 
-    def Reduce(self):
-        pass
+        filename = map_input["filename"]
+        start_index = map_input["startno"]
+        end_index = map_input["endno"]
+        keyword=map_input["keyword"]
+
+        # read file
+        f = open(filename, "r", encoding='utf-8')
+        text = f.read()
+        line = text[start_index:end_index]
+        words=line.split(' ')
+        count = 0
+        for word in words:
+            if word == keyword:
+                count+=1
+
+        return count
+
+
+    def Reduce(self,reduce_input):
+        totalCount = 0
+        for i in reduce_input:
+            totalCount += i
+
+        return totalCount

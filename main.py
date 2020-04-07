30596bbb1d1a0d57323aa08f8d1a612bebd132fb
ferhatyaman <ferhatyaman@sabanciuniv.edu>
2020-04-07 19:42:27 +0300

    Arguments are corrected.

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import sys
 if __name__ == '__main__':
 
     num_argv = len(sys.argv)
-    list_argv = str(sys.argv)
 
-    if num_argv == 4 and list_argv[1] == 'COUNT':
-        count_mapreduce = FindWordCount(int(list_argv[2]))
-        count_mapreduce.start(list_argv[3])
-    elif num_argv == 5 and list_argv[1] == 'FREQ':
-        count_mapreduce = FindWordFrequency(int(list_argv[2]))
-        count_mapreduce.start(list_argv[3], list_argv[4])
+    if num_argv == 4 and sys.argv[1] == 'COUNT':
+        count_mapreduce = FindWordCount(int(sys.argv[2]))
+        count_mapreduce.start(sys.argv[3])
+    elif num_argv == 5 and sys.argv[1] == 'FREQ':
+        count_mapreduce = FindWordFrequency(int(sys.argv[2]))
+        count_mapreduce.start(sys.argv[3], sys.argv[4])
     else:
         print('You have entered wrong inputs, check your command and parameters')
